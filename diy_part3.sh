98993f4fbf5f83e205dc59c7a4843f13df03094a
AK201314 <88940210+AK201314@users.noreply.github.com>
2023-05-07 15:13:43 +0800

    Create diy_part3.sh

---
+++ diy_part3.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
+
+
+#22.03 golong
+
+pushd feeds/packages/lang
+
+rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang
+
+popd
+
+
+# 删除原来的adbyby
+#rm -rf  feeds/extra/luci-app-adbyby-plus
+
+#svn co https://github.com/NueXini/NueXini_Packages/trunk/luci-app-diskman package/luci-app-diskman
+
+#svn co https://github.com/shidahuilang/openwrt-package/trunk/luci-app-autotimeset package/luci-app-autotimeset
+
+#svn co https://github.com/shidahuilang/openwrt-package/trunk/luci-app-adbyby-plus package/luci-app-adbyby-plus
+
+
+
+# 删除docker
+#rm -rf feeds/extra/luci-lib-docker
+#rm -rf feeds/extra/luci-app-docker
+#rm -rf feeds/luci/applications/luci-app-adbyby-plus
+rm -rf feeds/luci/applications/luci-app-dockerman
+
+#git clone https://github.com/shidahuilang/openwrt-package.git package/openwrt-package
+#rm -rf package/openwrt-package/luci-app-ssr-plus
+#git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+#git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+
+git clone https://github.com/AK201314/pack.git package/openwrt-package
+
+# 安装Docker
+#svn co https://github.com/NueXini/NueXini_Packages/trunk/luci-app-docker package/luci-app-docker
+
+git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
+
+# 添加cpu温度监控
+git clone https://github.com/AK201314/luci-app-temp-status.git package/luci-app-temp-status
+
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+
+#mv package/WiFiPortal/eqos-master-wiwiz/files/po/zh-cn package/WiFiPortal/eqos-master-wiwiz/files/po/zh_Hans
